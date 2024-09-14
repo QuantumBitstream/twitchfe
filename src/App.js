dc1d615262c8d7c6c8ba863ada5fdaac214b6086
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-09-15 05:59:11 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,37 +1,58 @@
-
-import React from 'react';
-import { Layout } from 'antd';
+import React, { useState } from 'react';
+import { Layout, message } from 'antd';
+import { logout } from './utils';
+import PageHeader from './components/PageHeader';
 
 
 const { Header, Content, Sider } = Layout;
 
 
 function App() {
-  return (
-      <Layout>
-        <Header>
-          {'Header'}
-        </Header>
+    const [loggedIn, setLoggedIn] = useState(false)
+
+
+    const signinOnSuccess = () => {
+        setLoggedIn(true);
+    }
+
+
+    const signoutOnClick = () => {
+        logout().then(() => {
+            setLoggedIn(false)
+            message.success('Successfully Signed out')
+        }).catch((err) => {
+            message.error(err.message)
+        })
+    }
+
+
+    return (
         <Layout>
-          <Sider width={300} className="site-layout-background">
-            {'Sider'}
-          </Sider>
-          <Layout style={{ padding: '24px' }}>
-            <Content
-                className="site-layout-background"
-                style={{
-                  padding: 24,
-                  margin: 0,
-                  height: 800,
-                  overflow: 'auto'
-                }}
-            >
-              {'Home'}
-            </Content>
-          </Layout>
+            <PageHeader
+                loggedIn={loggedIn}
+                signoutOnClick={signoutOnClick}
+                signinOnSuccess={signinOnSuccess}
+            />
+            <Layout>
+                <Sider width={300} className="site-layout-background">
+                    {'Sider'}
+                </Sider>
+                <Layout style={{ padding: '24px' }}>
+                    <Content
+                        className="site-layout-background"
+                        style={{
+                            padding: 24,
+                            margin: 0,
+                            height: 800,
+                            overflow: 'auto'
+                        }}
+                    >
+                        {'Home'}
+                    </Content>
+                </Layout>
+            </Layout>
         </Layout>
-      </Layout>
-  )
+    )
 }
 
 
