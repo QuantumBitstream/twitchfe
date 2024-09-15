dbc33bb00bd740922f8a0253875c6de96c1a8255
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-09-16 05:35:34 +0800

    update

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,92 +3,84 @@ import React, { useState } from 'react';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { register } from '../utils';
 
-
 function Register() {
-    const [displayModal, setDisplayModal] = useState(false)
+  const [displayModal, setDisplayModal] = useState(false)
 
+  const handleCancel = () => {
+    setDisplayModal(false)
+  }
 
-    const handleCancel = () => {
+  const signupOnClick = () => {
+    setDisplayModal(true)
+  }
+
+  const onFinish = (data) => {
+    register(data)
+      .then(() => {
         setDisplayModal(false)
-    }
+        message.success('Successfully signed up');
+      }).catch((err) => {
+        message.error(err.message);
+      })
+  }
 
-
-    const signupOnClick = () => {
-        setDisplayModal(true)
-    }
-
-
-    const onFinish = (data) => {
-        register(data)
-            .then(() => {
-                setDisplayModal(false)
-                message.success('Successfully signed up');
-            }).catch((err) => {
-            message.error(err.message);
-        })
-    }
-
-
-    return (
-        <>
-            <Button shape="round" type="primary" onClick={signupOnClick}>
-                Register</Button>
-            <Modal
-                title="Register"
-                visible={displayModal}
-                onCancel={handleCancel}
-                footer={null}
-                destroyOnClose={true}
-            >
-                <Form
-                    name="normal_register"
-                    initialValues={{ remember: true }}
-                    onFinish={onFinish}
-                    preserve={false}
-                >
-                    <Form.Item
-                        name="username"
-                        rules={[{ required: true, message: 'Please input your Username!' }]}
-                    >
-                        <Input prefix={<UserOutlined />} placeholder="Username" />
-                    </Form.Item>
-                    <Form.Item
-                        name="password"
-                        rules={[{ required: true, message: 'Please input your Password!' }]}
-                    >
-                        <Input
-                            prefix={<LockOutlined />}
-                            placeholder="Password"
-                        />
-                    </Form.Item>
-                    <Form.Item
-                        name="first_name"
-                        rules={[{ required: true, message: 'Please input your Firstname!' }]}
-                    >
-                        <Input
-                            placeholder="firstname"
-                        />
-                    </Form.Item>
-                    <Form.Item
-                        name="last_name"
-                        rules={[{ required: true, message: 'Please input your Lastname!' }]}
-                    >
-                        <Input
-                            placeholder="lastname"
-                        />
-                    </Form.Item>
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit">
-                            Register
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </Modal>
-        </>
-
-
-    )
+  return (
+    <>
+      <Button shape="round" type="primary" onClick={signupOnClick}>
+        Register</Button>
+      <Modal
+        title="Register"
+        visible={displayModal}
+        onCancel={handleCancel}
+        footer={null}
+        destroyOnClose={true}
+      >
+        <Form
+          name="normal_register"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          preserve={false}
+        >
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: 'Please input your Username!' }]}
+          >
+            <Input prefix={<UserOutlined />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Please input your Password!' }]}
+          >
+            <Input
+              prefix={<LockOutlined />}
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item
+            name="first_name"
+            rules={[{ required: true, message: 'Please input your Firstname!' }]}
+          >
+            <Input
+              placeholder="firstname"
+            />
+          </Form.Item>
+          <Form.Item
+            name="last_name"
+            rules={[{ required: true, message: 'Please input your Lastname!' }]}
+          >
+            <Input
+              placeholder="lastname"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
+  )
 }
-
 
 export default Register;
